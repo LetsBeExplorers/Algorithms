a33e4cdd5a54ec42d9a2f24d10eaa21482c01eba
raytracer <ray3618@gmail.com>
2024-11-17 11:40:12 -0700

    Added tests and plots for small weights

--- Project3/project3.py
+++ Project3/project3.py
@@ -149,29 +149,37 @@ def measure_execution_times(weights, values, capacity):
     return bottom_up_time, top_down_time
 
 # Plotting random input performance for fixed W
-def plot_performance_fixedW(capacity, bottom_up_times, top_down_times):
+def plot_performance_fixedW(capacity, bottom_up_times, top_down_times, smallwts=False):
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.plot(range(1, n_top+1), bottom_up_times, label=f'Bottom-Up')
     ax.plot(range(1, n_top+1), top_down_times, label=f'Top-Down')
     ax.set_xlabel('n values')
     ax.set_ylabel('Execution Time (seconds)')
-    ax.set_title('Execution Time of Knapsack Algorithms for Capacity = ' + str(capacity))
     ax.legend()
     plt.grid(True)
-    plt.savefig('plot_fixedW_'+ str(capacity) + '.png')
+    if not smallwts:
+        ax.set_title('Execution Time of Knapsack Algorithms for Capacity = ' + str(capacity))
+        plt.savefig('plot_fixedW_'+ str(capacity) + '.png')
+    else:
+        ax.set_title('Execution Time of Knapsack Algorithms for Capacity = ' + str(capacity) + ' and Small Weights')
+        plt.savefig('plot_fixedW_'+ str(capacity) + '_smallwts.png')
     plt.close(fig)
 
 # Plotting random input performance for fixed n
-def plot_performance_fixedn(n, bottom_up_times, top_down_times):
+def plot_performance_fixedn(n, bottom_up_times, top_down_times,smallwts=False):
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.plot(range(50, capacity_top+1, 5), bottom_up_times, label=f'Bottom-Up')
     ax.plot(range(50, capacity_top+1, 5), top_down_times, label=f'Top-Down')
     ax.set_xlabel('capacity values')
     ax.set_ylabel('Execution Time (seconds)')
-    ax.set_title('Execution Time of Knapsack Algorithms for n = ' + str(n))
     ax.legend()
     plt.grid(True)
-    plt.savefig('plot_fixedn_'+ str(n) + '.png')
+    if not smallwts:
+        ax.set_title('Execution Time of Knapsack Algorithms for n = ' + str(n))
+        plt.savefig('plot_fixedn_'+ str(n) + '.png')
+    else:
+        ax.set_title('Execution Time of Knapsack Algorithms for n = ' + str(n) + ' and Small Weights')
+        plt.savefig('plot_fixedn_'+ str(n) + '_smallwts.png')
     plt.close(fig)
 
 # Fixed test parameters
@@ -197,3 +205,48 @@ print("Fixed-n timed tests successfully written to files")
 #***End of Deliverable 2***
 
 #***Start of Deliverable 3***
+# Function to measure execution time for small weights
+def test_execution_times_smallwts(value, fixedW=True):
+    bottom_up_times = []
+    top_down_times = []
+    
+    if fixedW:
+        capacity = value
+        for n in range(1, n_top+1):
+            weights = newIntArrayCeiling(n, 11)
+            values = newIntArrayCeiling(n, 25)
+
+            # Measure execution times
+            bu_time, td_time = measure_execution_times(weights, values, capacity)
+            bottom_up_times.append(bu_time)
+            top_down_times.append(td_time)
+
+    # fixed n
+    else: 
+        n = value
+        for capacity in range(50, capacity_top+1, 5):
+            weights = newIntArrayCeiling(n, 11)
+            values = newIntArrayCeiling(n, 25)
+
+            # Measure execution times
+            bu_time, td_time = measure_execution_times(weights, values, capacity)
+            bottom_up_times.append(bu_time)
+            top_down_times.append(td_time)
+    
+    return bottom_up_times, top_down_times
+
+# Generate data and plots for fixed W - small weights
+for capacity in capacity_values:
+    n_top = 100
+    bottom_up_times_W_smallwts, top_down_times_W_smallwts = test_execution_times(capacity)
+    plot_performance_fixedW(capacity, bottom_up_times_W_smallwts, top_down_times_W_smallwts, True)
+
+print("Fixed-W timed tests with small weights successfully written to files")
+
+# Generate data and plots for fixed n - small weights
+for n in n_values:
+    capacity_top = 500
+    bottom_up_times_n_smallwts, top_down_times_n_smallwts = test_execution_times(n, False)
+    plot_performance_fixedn(n, bottom_up_times_n_smallwts, top_down_times_n_smallwts, True)
+
+print("Fixed-n timed tests with small weights successfully written to files")
